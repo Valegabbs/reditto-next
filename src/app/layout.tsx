7628342765727840ea2dd879397805f95029fd9a
Vitorell0 <enzovitorello@perfectpay.com.br>
2025-10-02 10:22:59 -0300

    fix: adiciona modulos importantes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/contexts/AuthContext";
-import { Analytics } from "@vercel/analytics/next";
+import { VercelAnalytics } from "./analytics-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,7 +29,7 @@ export default function RootLayout({
         <AuthProvider>
           {children}
         </AuthProvider>
-        <Analytics />
+        <VercelAnalytics />
       </body>
     </html>
   );
